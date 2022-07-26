083a7537bdc3444ac0927aaa9dbe31e009019ed7
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-07-26 11:54:19 +0200

    Legg til template aktiviteter

--- app/src/main/kotlin/domene/Aktivitet.kt
+++ app/src/main/kotlin/domene/Aktivitet.kt
@@ -1,11 +1,39 @@
 package domene
 
+enum class AktivitetsType { Øvelse, Kurs, Oppgave }
+
 class Aktivitet(
     private val tittel: String,
     private val beskrivelse: String,
     private val type: AktivitetsType,
     private val mål: String
 ) {
-    enum class AktivitetsType { Øvelse, Kurs, Oppgave }
 }
+
+val aktiviteter: List<Aktivitet> = listOf(
+    Aktivitet(
+        tittel = "Pilotering av medarbeidersamtalen",
+        beskrivelse = "Svada svada",
+        type = AktivitetsType.Øvelse,
+        mål = "Å bli ferdig"
+    ),
+    Aktivitet(
+        tittel = "Kartleggingsmøte med ansatt",
+        beskrivelse = "Enda mer svada",
+        type = AktivitetsType.Oppgave,
+        mål = "Å bli (nesten) ferdig"
+    ),
+    Aktivitet(
+        tittel = "Sinnemestring",
+        beskrivelse = "Hvordan ikke bli sinna?",
+        type = AktivitetsType.Kurs,
+        mål = "Ikke være sinna mer"
+    ),
+    Aktivitet(
+        tittel = "Hvordan ta den vanskelige praten?",
+        beskrivelse = "Vi hjelper deg på veien.",
+        type = AktivitetsType.Kurs,
+        mål = "Udefinert"
+    ),
+)
 
