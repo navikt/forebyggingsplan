b805af30df1244397b9d543dd08cb99309411706
Christian Klem <christian.klem@nav.no>
2022-09-19 12:24:07 +0200

    Åpner metrics endepunkt
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- app/src/main/kotlin/Main.kt
+++ app/src/main/kotlin/Main.kt
@@ -72,6 +72,7 @@ fun bootstrapServer() {
         }
         routing {
             helseEndepunkter()
+            metrics()
             authenticate("tokenx") {
                 aktivitetsmaler(aktivitetService = aktivitetService)
                 medAltinnTilgang {
