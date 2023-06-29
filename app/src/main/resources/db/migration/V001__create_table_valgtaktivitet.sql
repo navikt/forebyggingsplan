1997ce0fb8284b674699ba9f8c75a1c505012c0f
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-06-29 14:18:43 +0200

    o -> oe

--- app/src/main/resources/db/migration/V001__create_table_valgtaktivitet.sql
+++ app/src/main/resources/db/migration/V001__create_table_valgtaktivitet.sql
@@ -4,6 +4,6 @@ create table valgtaktivitet
     aktivitetsmal_id    varchar(40),
     virksomhetsnummer   varchar(20),
     fødselsnummer       varchar(11),
-    fullfort           boolean   default false,
-    fullfort_tidspunkt timestamp default null
+    fullfoert           boolean   default false,
+    fullfoert_tidspunkt timestamp default null
 );
