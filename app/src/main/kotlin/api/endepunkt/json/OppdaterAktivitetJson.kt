139983de2f5e0d857b1fb5fe900ba90305deb240
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-10-02 14:21:25 +0200

    Fiks test

--- app/src/main/kotlin/api/endepunkt/json/OppdaterAktivitetJson.kt
+++ app/src/main/kotlin/api/endepunkt/json/OppdaterAktivitetJson.kt
@@ -8,13 +8,16 @@ data class OppdaterAktivitetJson(
     val aktivitetstype: Aktivitetstype?,
     val status: String
 ) {
-    fun tilDomene(hashetFodselsnummer: ByteArray, orgnr: String, aktivitetsid: String): Aktivitet = when (aktivitetstype) {
-        Aktivitetstype.TEORISEKSJON -> Aktivitet.Teoriseksjon(
-            hashetFodselsnummer, orgnr, aktivitetsid, Aktivitet.Teoriseksjon.Status.valueOf(status)
-        )
-        Aktivitetstype.OPPGAVE -> Aktivitet.Oppgave(
-            hashetFodselsnummer, orgnr, aktivitetsid, Aktivitet.Oppgave.Status.valueOf(status)
-        )
-        null -> Aktivitet.Oppgave(hashetFodselsnummer, orgnr, aktivitetsid, Aktivitet.Oppgave.Status.valueOf(status))
+    fun tilDomene(hashetFodselsnummer: ByteArray, orgnr: String, aktivitetsid: String): Aktivitet {
+        val statusUppercase = status.uppercase()
+        return when (aktivitetstype) {
+            Aktivitetstype.TEORISEKSJON -> Aktivitet.Teoriseksjon(
+                hashetFodselsnummer, orgnr, aktivitetsid, Aktivitet.Teoriseksjon.Status.valueOf(statusUppercase)
+            )
+            Aktivitetstype.OPPGAVE -> Aktivitet.Oppgave(
+                hashetFodselsnummer, orgnr, aktivitetsid, Aktivitet.Oppgave.Status.valueOf(statusUppercase)
+            )
+            null -> Aktivitet.Oppgave(hashetFodselsnummer, orgnr, aktivitetsid, Aktivitet.Oppgave.Status.valueOf(statusUppercase))
+        }
     }
 }
