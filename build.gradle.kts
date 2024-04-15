c4f0751ae416bfcda9afafc379a4989d9d266021
Thomas Dufourd <thomas.dufourd@gmail.com>
2024-04-15 11:01:06 +0200

    Upgrade kotlin (patch 1.9.22 -> 1.9.23)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    kotlin("jvm") version "1.9.22" apply false
+    kotlin("jvm") version "1.9.23" apply false
     kotlin("plugin.serialization") version "1.9.22" apply false
 }
 
