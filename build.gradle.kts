37ec5ae02226f2fe4a06f638cd537246522fbbec
Christian Klem <christian.klem@nav.no>
2024-11-12 15:34:51 +0100

    Oppdater gradle og kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
     java
-    kotlin("jvm") version "2.0.20" apply false
-    kotlin("plugin.serialization") version "2.0.20" apply false
+    kotlin("jvm") version "2.0.21" apply false
+    kotlin("plugin.serialization") version "2.0.21" apply false
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
