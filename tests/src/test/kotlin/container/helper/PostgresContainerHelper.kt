920a1666c58270fc00a12f80761f871aff604f81
nimajimale <nima0512@gmail.com>
2026-05-20 15:52:33 +0200

    [WIP]: Oppgrader fra POSTGRES_15 til POSTGRES_16 og deploy til dev

--- tests/src/test/kotlin/container/helper/PostgresContainerHelper.kt
+++ tests/src/test/kotlin/container/helper/PostgresContainerHelper.kt
@@ -22,7 +22,7 @@ class PostgresContainerHelper(
     private var migreringErKjørt = false
     private val port = 5432
     val container: PostgreSQLContainer =
-        PostgreSQLContainer(DockerImageName.parse("postgres:15.18"))
+        PostgreSQLContainer(DockerImageName.parse("postgres:16.14"))
             .withLogConsumer(
                 Slf4jLogConsumer(log).withPrefix(postgresNetworkAlias).withSeparateOutputStreams(),
             )
